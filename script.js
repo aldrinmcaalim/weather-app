51ce82e080361ec2e5bfb9e57ea4162c431bf238
Aldrin Caalim <58124048+aldrinmcaalim@users.noreply.github.com>
2020-10-11 19:27:25 -0400

    Adding longitue and latitude

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ window.addEventListener('load', () => {
     let latitude;
 
     if (navigator.geolocation) {
-
+        navigator.geolocation.getCurrentPosition(position => {
+            longitude = position.coords.longitude;
+            latitude = position.coords.latitude;
+        });
     } else {
         h1.textContent = "Turn on the location for the application to work"
     }
