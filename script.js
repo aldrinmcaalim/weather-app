774b6da9aff9f3534ae8d1c270c45465773fd8ad
Aldrin Caalim <58124048+aldrinmcaalim@users.noreply.github.com>
2020-10-11 19:25:14 -0400

    Adding location monitor

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.addEventListener('load', () => {
     let latitude;
 
     if (navigator.geolocation) {
-        
+
+    } else {
+        h1.textContent = "Turn on the location for the application to work"
     }
 });
